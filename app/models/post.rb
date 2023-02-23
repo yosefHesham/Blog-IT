42428e54f9b38f35aba171810225c5d1e28a3835
yosefHesham <youssef.h@trufla.com>
2023-02-23 20:50:34 +0200

    Add Methods for posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,17 +1,15 @@
 ## class Post
 class Post < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, class_name: 'User', foreign_key: 'user_id'
   has_many :comment
   has_many :like
 
-  def update_posts_counter(user)
-    Post.create(user: user, title: 'Hello', text: 'This is my fourth  post')
-    updatedUser = User.where('id = ?', params[user.id])
-    updatedUser.postCountr += 1
-    updatedUser.save
+  
+  def recent_comments
+    comments.order(created_at: :desc).limit(5)
   end
 
-  def recent_comments
-    Comments.where('post_id = ?', params[id]).order(created_at: :desc).limit(5)
+  def update_posts_counter
+    author.increment!(:posts_count)
   end
 end
